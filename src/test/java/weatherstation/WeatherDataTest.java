a87ace112639e9f4676b5c2a0b02df3e70f0a624
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2026-01-10 13:22:58 +0300

    feature : add an unit tests for the weather data
    * Added an unit tests for the weather data

---
+++ src/test/java/weatherstation/WeatherDataTest.java
@@ -0,0 +1,42 @@
+package weatherstation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import weatherstation.weather.observers.MockCelsiusDisplay;
+import weatherstation.weather.observers.MockCurrentConditionDisplay;
+import weatherstation.weather.subject.WeatherData;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WeatherDataTest {
+
+    private static final float TEMPERATURE = 80f;
+    private static final float HUMIDITY = 65f;
+    private static final float PRESSURE = 30.4f;
+
+    @DisplayName(value = "Should return an expected data, when test condition display.")
+    @Test
+    void shouldReturnExpectedDataWhenTestConditionDisplay() {
+        String expectedData = "(" + TEMPERATURE + ", " + HUMIDITY + ")";
+
+        WeatherData weatherData = new WeatherData();
+
+        MockCurrentConditionDisplay currentConditionsDisplay = new MockCurrentConditionDisplay(weatherData);
+        weatherData.setMeasurements(TEMPERATURE, HUMIDITY, PRESSURE);
+
+        assertThat(currentConditionsDisplay.getData()).isEqualTo(expectedData);
+    }
+
+    @DisplayName(value = "Should return celsius degree, when converting farenheit to celsius.")
+    @Test
+    void shouldReturnCelsiusDegreeWhenConvertingFarenheitToCelsius() {
+        String expectedData = "26.666666";
+        WeatherData weatherData = new WeatherData();
+
+        MockCelsiusDisplay mockCelsiusDisplay = new MockCelsiusDisplay(weatherData);
+        weatherData.setMeasurements(TEMPERATURE, HUMIDITY, PRESSURE);
+
+        assertThat(mockCelsiusDisplay.getData()).isEqualTo(expectedData);
+    }
+
+}
