e8b9d0ad4ddaf16539b2ffdf00340dd6822dce7b
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2026-01-13 13:08:24 +0300

    refactor : edit a javadoc for the condiment decorator
    * Edited a JavaDoc for the condiment decorator

--- src/main/java/starbuzz/decorator/condiments/CondimentDecorator.java
+++ src/main/java/starbuzz/decorator/condiments/CondimentDecorator.java
@@ -2,6 +2,11 @@ package starbuzz.decorator.condiments;
 
 import starbuzz.decorator.beverage.Beverage;
 
+/**
+ * @author Mikhail
+ * <p>Абстрактный класс для дополнений.</p>
+ * Объекты должны быть взаимозаменяемы с Beverage, поэтому используется наследование.
+ */
 public abstract class CondimentDecorator extends Beverage {
 
     protected Beverage beverage;
