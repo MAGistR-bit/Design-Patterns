7491eef1334a5bc83f3e9d81eb0c727c0520c4c3
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2026-01-18 12:57:53 +0300

    feature : add a thick crust dough
    * Added a thick crust dough

---
+++ src/main/java/factory/abstractFactory/ingredients/dough/ThickCrustDough.java
@@ -0,0 +1,9 @@
+package factory.abstractFactory.ingredients.dough;
+
+public class ThickCrustDough extends Dough {
+
+    @Override
+    public String toString() {
+        return "ThickCrust style extra thick crust dough";
+    }
+}
