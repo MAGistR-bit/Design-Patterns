ac01606bac859adb3f7fc2f77a4122cb1101d483
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2026-01-18 12:56:17 +0300

    feature : add an implementation for the chicago pizza store
    * Added an implementation for the Chicago Pizza store

--- src/main/java/factory/abstractFactory/stores/ChicagoPizzaStore.java
+++ src/main/java/factory/abstractFactory/stores/ChicagoPizzaStore.java
@@ -1,4 +1,40 @@
 package factory.abstractFactory.stores;
 
-public class ChicagoPizzaStore {
+import factory.abstractFactory.PizzaIngredientFactory;
+import factory.abstractFactory.ingredients.factory.chicagoFactory.ChicagoIngredientFactory;
+import factory.abstractFactory.pizza.CheesePizza;
+import factory.abstractFactory.pizza.ClamPizza;
+import factory.abstractFactory.pizza.PepperoniPizza;
+import factory.abstractFactory.pizza.Pizza;
+import factory.abstractFactory.pizza.VeggiePizza;
+
+public class ChicagoPizzaStore extends PizzaStore {
+
+    @Override
+    Pizza createPizza(String type) {
+        Pizza pizza;
+        PizzaIngredientFactory ingredientFactory = new ChicagoIngredientFactory();
+
+        switch (type) {
+            case "cheese" -> {
+                pizza = new CheesePizza(ingredientFactory);
+                pizza.setName("Chicago Style Cheese Pizza");
+            }
+            case "pepperoni" -> {
+                pizza = new PepperoniPizza(ingredientFactory);
+                pizza.setName("Chicago Style Pepperoni Pizza");
+            }
+            case "clam" -> {
+                pizza = new ClamPizza(ingredientFactory);
+                pizza.setName("Chicago Style Clam Pizza");
+            }
+            case "veggie" -> {
+                pizza = new VeggiePizza(ingredientFactory);
+                pizza.setName("Chicago Style Veggie Pizza");
+            }
+            default -> throw new IllegalArgumentException("Unsupported pizza type.");
+        }
+
+        return pizza;
+    }
 }
