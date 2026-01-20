784a4217a58442d33b4e9be24d09c9c6c472c6d1
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2026-01-20 13:05:37 +0300

    update : edit a singleton class
    * Edited the Singleton class

--- src/main/java/singleton/Singleton.java
+++ src/main/java/singleton/Singleton.java
@@ -1,8 +1,29 @@
 package singleton;
 
+/**
+ * @author Mikhail
+ * <p>Классическая реализация паттерна Одиночка.</p>
+ */
 public final class Singleton {
 
+    /**
+     * Статическая переменная для хранения единственного экземпляра.
+     */
+    private static Singleton uniqueInstance;
+
+    /**
+     * Приватный конструктор.
+     * <p>Только Singleton
+     * может создавать экземпляры этого класса.</p>
+     */
     private Singleton() {
 
+    }
+
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
     }
 }
