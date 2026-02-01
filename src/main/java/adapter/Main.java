a16d6aa123ff7528a1d9a537e603870782637176
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2026-02-01 17:05:59 +0300

    feature : add a classes for the adapter design pattern
    * Added a java classes for the adapter design pattern

--- src/main/java/adapter/Main.java
+++ src/main/java/adapter/Main.java
@@ -2,6 +2,20 @@ package adapter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        TurkeyAdapter adapter = new TurkeyAdapter(new Turkey() {
+            @Override
+            public void gobble() {
+                System.out.println("I'm gobble");
+            }
+
+            @Override
+            public void fly() {
+                System.out.println("I'm flying");
+            }
+        });
+
+        adapter.quack();
+        adapter.fly();
     }
 }
---
+++ src/main/java/adapter/TurkeyAdapter.java
@@ -0,0 +1,31 @@
+package adapter;
+
+/**
+ * @author Mikhail
+ * <p><b>Паттерн Адаптер.</b></p>
+ * <p>Реализуем интерфейс того типа, на который
+ * рассчитан клиент. В нашем случае - интерфейс {@code Duck}.</p>
+ */
+public class TurkeyAdapter implements Duck {
+    private final Turkey turkey;
+
+    /**
+     * Получаем ссылку на адаптируемый объект в конструкторе.
+     * @param turkey индейка (ссылка на адаптируемый объект).
+     */
+    public TurkeyAdapter(Turkey turkey) {
+        this.turkey = turkey;
+    }
+
+    @Override
+    public void quack() {
+        turkey.gobble();
+    }
+
+    @Override
+    public void fly() {
+        for (int i = 0; i < 5; i++) {
+            turkey.fly();
+        }
+    }
+}
