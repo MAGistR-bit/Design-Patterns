3db9ac3b654fb4f4367fed6bb63ca8beba55e81a
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2026-02-04 13:22:21 +0300

    update : update a version for the assertj-core
    * Updated a version for the AsserJ Core

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.27.6")
+    testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
 tasks.test {
